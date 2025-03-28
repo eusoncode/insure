f04341917859d9fa69ee804bd62c1cf113effe01
eusta <eusoncode@gmail.com>
2025-03-28 00:06:57 -0600

    Added the footer

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,33 +1,116 @@
-
-
+import CustomLink from "@/lib/Helper_function/links";
+import Image from "next/image";
+import logo from "@/assets/logo-light.png";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col border-2 border-blue-600 min-h-175 bg-gray-800 text-white py-10">
-      <div className="flex flex-col container mx-auto px-6">
+    <footer className="flex justify-center align-center min-h-175 bg-custom-blue text-white py-10">
+      <div className="container flex flex-col justify-between mt-16 mx-auto border-2 border-transparent">
         {/* Footer box for further inqury */}
-        <div className="flex md:flex-row justify-between items-start space-y-0">
-          {/* Logo */}
-          <div className="border-2 border-blue-600">Logo</div>
+        <div className="flex flex-row mt-16 space-x-2 justify-between items-start space-y-0">
+          {/* Logo and socials */}
+          <div className="flex flex-col max-w space-y-2 justify-start items-start">
+            {/* Logo */}
+            <div>
+              <CustomLink href="/home" className="flex mb-4">
+                <Image src={logo} alt="logo" priority width={200} />
+              </CustomLink>
+            </div>
+            <h3 className="flex flex-wrap font-sans font-bold text-xl">
+              Insurance that reflects the way <br /> you live your life.
+            </h3>
+            <div className="flex flex-row mt-4 space-x-6">
+              <div className="flex justify-center items-center text-lg text-white border-1 rounded-4xl w-8 h-8 bg-socials-gray border-transparent">
+                t
+              </div>
+              <div className="flex justify-center items-center text-lg text-white border-1 rounded-4xl w-8 h-8 bg-socials-gray border-transparent">
+                f
+              </div>
+              <div className="flex justify-center items-center text-lg text-white border-1 rounded-4xl w-8 h-8 bg-socials-gray border-transparent">
+                p
+              </div>
+              <div className="flex justify-center items-center text-lg text-white border-1 rounded-4xl w-8 h-8 bg-socials-gray border-transparent">
+                @
+              </div>
+            </div>
+          </div>
 
           {/* Contact */}
-          <div>Contact</div>
+          <div className="flex flex-col justify-start items-start space-y-6">
+            <h2 className="flex flex-row space-x-4 justify-center items-center font-bold text-[20px] leading-[24px]">
+              <p>Contact</p>
+              <div className="border-2 border-base-didi w-6 rounded-2xl"></div>
+            </h2>
+            <p className="font-sans text-insure-text-dark">
+              88 Broklyn Golden Road Street, <br /> New York. USA
+            </p>
+            <div className="flex flex-col justify-center items-start space-y-2 mt-4">
+              <div className="flex flex-row space-x-4">
+                <i>@</i>
+                <p>+92(8800)-6930</p>
+              </div>
+              <div className="flex flex-row space-x-4">
+                <i>@</i>
+                <p>needhelp@company.com</p>
+              </div>
+            </div>
+          </div>
 
           {/* Instagram */}
-          <div>Instagram</div>
+          <div className="flex flex-col justify-start items-start space-y-6 max-w-sm">
+            <h2 className="flex flex-row space-x-4 justify-center items-center font-bold text-[20px] leading-[24px]">
+              <p>Instagram</p>
+              <div className="border-2 border-base-didi w-6 rounded-2xl"></div>
+            </h2>
+            <ul className="container flex flex-wrap flex-row w-full space-x-2 mt-4 space-y-2">
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+              <li className="h-24 w-29 rounded-sm bg-gray-400 border-2 border-transparent"></li>
+            </ul>
+          </div>
 
           {/* Newsletter */}
-          <div>Newsletter</div>
+          <div className="flex flex-col justify-start items-start space-y-6">
+            <h2 className="flex flex-row space-x-4 justify-center items-center font-bold text-[20px] leading-[24px]">
+              <p>Newletter</p>
+              <div className="border-2 border-base-didi w-6 rounded-2xl"></div>
+            </h2>
+            <p className="font-sans text-insure-text-dark">
+              Subscribe our newsletter to get <br /> our latest update & news.
+            </p>
+            <form
+              action="#"
+              method="POST"
+              className="container mx-auto flex border-2 border-blue-600"
+            >
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address"
+                className="flex justify-center items-center pl-8 pr-12 bg-white font-bold text-black"
+              />
+              <button
+                type="submit"
+                className="px-1 py-6 bg-base-didi border-2 border-blue-600"
+              >
+                <span>Submit</span>
+              </button>
+            </form>
+          </div>
         </div>
         {/* Footer sticks to bottom */}
-        <div className="py-4 text-center text-sm text-gray-500">
-          <p>&copy; 2025 Insure. All rights reserved.</p>
+        <div className="border-2 border-transparent bg-custom-darker-blue py-10">
+          <p className="font-sans font-medium text-[15px] leading-[23px] text-insure-text-dark text-center">
+            &copy; Copyright 2025 by Insure. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
   );
 }
-
 
 // export default function Footer() {
 //   return (
