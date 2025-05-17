bf67478638eb1c6836d20680c2ec4e82e72e0f61
eusta <eusoncode@gmail.com>
2025-05-16 23:14:14 -0600

    progress on the envato

--- frontend/src/components/body_section/envato_section.tsx
+++ frontend/src/components/body_section/envato_section.tsx
@@ -1,16 +1,35 @@
+import Image from "next/image";
+
 export default function Envato_section() {
   return (
-    <section className="relative flex justify-center items-center h-70 border border-[#F4F3F8]">
-      <div className="absolute top-0 left-0 w-40 h-40 border-2 border-red-700"></div>
-      <div className="absolute right-0 w-108 h-70 border-2 border-yellow-700"></div>
-      <div className="flex justify-center items-center w-300 h-18 gap-24 border border-purple-700">
+    <section className="relative flex justify-center items-center h-70 border border-[#E0DDEA]">
+      <div className="absolute top-0 left-0 w-105 h-30">
+        <Image
+          src="/envato/line-shape-1.png"
+          alt="envato-bg-1"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="absolute right-0 w-110 h-70">
+        <Image
+          src="/envato/line-shape-2.png"
+          alt="envato-bg-1"
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="flex justify-center items-center w-full h-25">
         {[...Array(5)].map((_, index) => (
-          <i
-            key={index}
-            className="flex justify-center items-center w-40 h-10 border border-blue-700"
-          >
-            envato
-          </i>
+          <div className="group flex justify-center items-center rounded-[10px] overflow-hidden w-[230px] h-[70px] hover:bg-gray-100 transition-all duration-500 ease-in-out">
+            <Image
+              src="/envato/brand-1-1.png"
+              alt="envato"
+              width={120}
+              height={120}
+              className="opacity-25 max-w-full group-hover:opacity-70 transition-all duration-500 ease-in-out"
+            />
+          </div>
         ))}
       </div>
     </section>
--- frontend/src/components/main_section.tsx
+++ frontend/src/components/main_section.tsx
@@ -1,5 +1,4 @@
 import Insurance_section from "@/components/body_section/insurance_page_section";
-import Save_money_section from "@/components/body_section/save_your_money_section";
 import About_company_section from "@/components/body_section/about_company_section";
 import Offering_section from "@/components/body_section/offering_section";
 import About_company2_section from "@/components/body_section/about_company2_section";
@@ -13,13 +12,12 @@ import Track_your_claim_section from "@/components/body_section/track_your_claim
 export default function Main_section() {
   return (
     <section className="">
-      <div className="flex flex-col mb-2 border-2 border-green-600">
+      <div className="flex flex-col border-2 border-green-600">
         <Insurance_section />
         <About_company_section />
         <Offering_section />
         <About_company2_section />
         <Envato_section />
-        <Save_money_section />
         <Free_quote_page_section />
         <Numbers_section />
         <Recent_portfolio_section />
