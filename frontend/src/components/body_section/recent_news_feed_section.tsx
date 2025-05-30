eb9b256de13738c85efae00409f7a3e4c755e229
eusta <eusoncode@gmail.com>
2025-05-30 00:45:11 -0600

    adding images

--- frontend/src/components/body_section/recent_news_feed_section.tsx
+++ frontend/src/components/body_section/recent_news_feed_section.tsx
@@ -1,65 +1,73 @@
 import { VscFolderOpened } from "react-icons/vsc";
+import Image from "next/image";
 
 export default function Recent_news_feed_section() {
   return (
     <section className="flex flex-col relative justify-center items-center border border-green-600">
       {/** Recent news feed section **/}
-      <div className="w-full h-200 border border-blue-600">
+      <div className="w-full h-250 border border-blue-600">
         {/** Caption **/}
-        <div className="border border-red-600">
+        <div className="flex flex-col justify-center items-center pt-4 mx-85 mb-4">
           {/** heading **/}
-          <div className="">
-            <p className=""> RECENT NEWS FEED</p>
-            <span className="bg-[#00CDE5]"></span>
+          <div className="relative">
+            <p className="uppercase font-semibold leading-[1.2em] text-[14px] tracking-widest text-[#82828a]">
+              recent news feed
+            </p>
+            <span className="absolute top-1/2 left-[110%] w-[17px] h-[3px] inline-block translate-y-[-50%] rounded-[1.5px] bg-[#00CDE5]"></span>
           </div>
           {/** main caption **/}
-          <div className="">
-            <p className="">Latest news & articles from the blog</p>
-          </div>
+          <span className="block w-full text-center text-[#2B2B5E] text-[45px] font-bold tracking-[-0.04em] leading-[1.1em] mt-2">
+            Latest news & articles <br /> from the blog
+          </span>
         </div>
 
-        {/** Images **/}
-        <div className="border border-red-600">
+        {/** Image section **/}
+        <div className="flex justify-center items-center gap-4 mx-85 pt-12 mt-10 mb-4 border border-red-600">
           {/** Images 1 **/}
-          <div className="">
-            {/**  **/}
-            <div className="flex flex-col justify-center items-center text-white w-20 h-18 bg-[#00CDE5]">
-              <p className="">28</p>
-              <p className="">nov</p>
+          <div className="relative w-[31%] h-125 border border-blue-600">
+            {/** Date icon **/}
+            <div className="absolute z-[10] top-[-10px] right-[30px] flex flex-col justify-center items-center text-center text-[10px] py-[20px] font-medium leading-[1.2em] tracking-widest text-white uppercase w-[50px] h-[58px] bg-[#00CDE5]">
+              <span className="text-[18px] mb-[5px]">28</span>
+              nov
             </div>
 
             {/** main image **/}
-            <div className=""></div>
+            <div
+              className="w-full h-[65%] border border-red-500"
+              style={{
+                backgroundImage: "url('/recent_news_feed/blog-1-1.jpg')",
+              }}
+            ></div>
 
             {/**  **/}
-            <div className="">
+            <div className="flex flex-col gap-4 absolute bottom-[-10] left-[20] bg-white w-83 h-57 shadow-sm border border-blue-600">
               {/**  **/}
-              <div className="">
-                <i></i>
+              <div className="flex mt-6 text-sm gap-4 pl-6">
+                <i>O</i>
                 <p>by Admin</p>
                 <p>|</p>
-                <i></i>
+                <i>O</i>
                 <p>2 Comments</p>
               </div>
 
               {/**  **/}
-              <p className="">
-                The quality role of the life <br /> insurance new policies in{" "}
+              <span className="font-bold text-[23px] pl-6 leading-[1.2em] py-2 text-[#2B2B5E]">
+                The quality role of the life <br /> insurance new policies in
                 <br /> company
-              </p>
+              </span>
 
               {/**  **/}
-              <div className="">
+              <div className="flex gap-4 pl-6 pt-2 border-t border-[#e0ddea]">
                 <p className="">Read More</p>
-                <i className=""></i>
+                <i className=""> -></i>
               </div>
             </div>
           </div>
 
           {/** Images 2 **/}
-          <div></div>
+          <div className="relative w-[31%] h-125 border border-blue-600"></div>
           {/** Images 3 **/}
-          <div></div>
+          <div className="relative w-[31%] h-125 border border-blue-600"></div>
         </div>
       </div>
 
